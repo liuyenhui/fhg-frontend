3e6f957a0b8cce72e13aa3ea984c42e5001821ad
Hui Liu <liuyenhui@gmail.com>
2025-11-27 17:10:02 +0800

    create github repository

---
+++ src/pages/index.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>Flux + Helm + GitHub GitOps</h1>
+      <p>Placeholder Next.js frontend for the fhg project.</p>
+    </main>
+  );
+}
